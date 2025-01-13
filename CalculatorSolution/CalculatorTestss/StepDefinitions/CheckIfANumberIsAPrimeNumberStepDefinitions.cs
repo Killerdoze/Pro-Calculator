2c7560bb1ba4453539f7d81f31514a2c38608d86
Killerdoze <medchrifmastouri@gmail.com>
2025-01-13 13:59:04 +0100

    Define definition for Primary Number functionality

---
+++ CalculatorSolution/CalculatorTestss/StepDefinitions/CheckIfANumberIsAPrimeNumberStepDefinitions.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using System;
+
+namespace CalculatorTestss.StepDefinitions  // Changez ici pour correspondre à votre structure de dossiers
+{
+    [Binding]
+    public class PrimeNumberSteps
+    {
+        private int _number;
+        private string _result;
+
+        [Given(@"the prime number is (\d+)")]
+        public void GivenTheNumberIs(int number)
+        {
+            _number = number;
+        }
+
+        [When(@"I check if the number is prime")]
+        public void WhenICheckIfTheNumberIsPrime()
+        {
+            _result = IsPrime(_number) ? "Prime" : "Not Prime";
+        }
+
+        [Then(@"the prime result should be ""(.*)""")]
+        public void ThenTheResultShouldBe(string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _result);
+        }
+
+        public string GetResult()
+        {
+            return _result;  // Retourne le résultat calculé
+        }
+
+        // Rendre la méthode IsPrime statique
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
